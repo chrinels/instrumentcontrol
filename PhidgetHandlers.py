2e0f3368970a885aa5b75e7105e044d6614103e0
Christian Nelson <christian.nelson@eit.lth.se>
2019-04-30 10:05:56 +0200

    Added velocity limit on the Stepper on attachment

--- PhidgetHandlers.py
+++ PhidgetHandlers.py
@@ -3,6 +3,7 @@
 def on_attach_handler(channel):
     channel.setDataInterval(250)  # 250 ms
     channel.setRescaleFactor(1 / 3200)  # 1 now equals full rotation
+    channel.setVelocityLimit(1.5)
     channel.setEngaged(True)
     print('\x1b[1;34mPhidget::SN::{}\x1b[0m < Attached'.format(channel.getDeviceSerialNumber()))
 
